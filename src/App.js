18e6a54b9a2b9cc11b91d4d904978300687900e6
Sakshee1104 <saksheesharma1104@gmail.com>
2024-10-10 19:18:05 +0530

    working on WhyChoosUs page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
-import About from './component/pages/About';
-import Home from './component/pages/Home';
+import "./App.css";
+import About from "./component/pages/About";
+import Home from "./component/pages/Home";
+import ChooseUs from "./component/pages/ChooseUs";
 
 function App() {
   return (
     <>
-    <Home/>
-    <About/>
+      <Home />
+      <About />
+      <ChooseUs />
     </>
   );
 }
