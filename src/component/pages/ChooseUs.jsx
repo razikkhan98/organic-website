4ff0002d2031d467f985728db1cc4331041df387
Sakshee1104 <saksheesharma1104@gmail.com>
2024-10-11 13:20:21 +0530

    why choose us page completed

--- src/component/pages/ChooseUs.jsx
+++ src/component/pages/ChooseUs.jsx
@@ -1,38 +1,72 @@
 import React from "react";
-import Jar from "../img/jar.png";
-import Icon1 from "../img/WhuChooseUS/icon1.png";
+import Jar from "../img/WhyChooseUs/jar.png";
+import Icon1 from "../img/WhyChooseUs/icon1.png";
+import Icon2 from "../img/WhyChooseUs/icon2.png";
+import Icon3 from "../img/WhyChooseUs/icon3.png";
+import Icon4 from "../img/WhyChooseUs/icon4.png";
+import Icon5 from "../img/WhyChooseUs/icon5.png";
 import { Container, Row, Col } from "react-bootstrap";
 const ChooseUs = () => {
-  const InfoWithIcon = () => {
+  const InfoWithIcon = [
     {
-      img: Icon1;
-      title: "";
-    }
-  };
+      img: Icon1,
+      title:
+        " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, odit.",
+    },
+    {
+      img: Icon2,
+      title:
+        " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, odit.",
+    },
+    {
+      img: Icon3,
+      title:
+        " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, odit.",
+    },
+    {
+      img: Icon4,
+      title:
+        " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, odit.",
+    },
+    {
+      img: Icon5,
+      title:
+        " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, odit.",
+    },
+  ];
   return (
     <>
       {/* Why choose us section start */}
       <Container>
-        <div className="WhyChooseUs">
+        <div className="WhyChooseUs p-3">
           <div>
             <Row>
               <Col>
-                <img src={Jar} className="WhyChooseUs-leftimg" />
+                <img src={Jar} className="h-75" />
               </Col>
               <Col>
-                <div className="WhyHeading">Why Choose Us ?</div>
+                <div className="WhyHeading mt-5">Why Choose Us ?</div>
+                <p className="mt-5">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
+                  accusamus reprehenderit, magni sequi eaque nisi magnam sint
+                  explicabo quod aperiam?
+                </p>
               </Col>
               <Col>
-                <img src={Jar} className="WhyChooseUs-text" />
+                <img src={Jar} className="h-75" />
               </Col>
             </Row>
           </div>
-          <Row>
-            <p className="WhyChooseUs-text">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
-              accusamus reprehenderit, magni sequi eaque nisi magnam sint
-              explicabo quod aperiam?
-            </p>
+          <Row></Row>
+          <Row className="mt-3">
+            {InfoWithIcon.map((item, index) => (
+              <Col key={index} className="mt-3">
+                <img src={item.img} className="WhyChooseUs-icons" />
+                <Row>
+                  <div className="mt-3">{item.title}</div>
+                </Row>
+              </Col>
+            ))}
           </Row>
         </div>
       </Container>
